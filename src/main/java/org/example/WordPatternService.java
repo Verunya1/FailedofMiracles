6fc26cc383bbbb6fd8b2295aea21d20169357c96
vera3 <vera3899@gmail.com>
2023-10-12 14:55:53 +0300

    game

---
+++ src/main/java/org/example/WordPatternService.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class WordPatternService {
+
+    public static String revealLetter(String encodedWord,String currentPattern, char letter){
+        encodedWord = encodedWord.toLowerCase();
+        StringBuilder revealedPattern = new StringBuilder(currentPattern);
+
+        for (int i = 0; i < encodedWord.length(); i++) {
+            if (encodedWord.charAt(i) == letter) {
+                revealedPattern.setCharAt(i, letter);
+            }
+        }
+
+        return revealedPattern.toString();
+    }
+
+    public static boolean tryGuessWord(String encodedWord, String guessedWord){
+        return guessedWord.equals(encodedWord);
+    }
+}
+
